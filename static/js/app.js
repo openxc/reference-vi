91788571fbb28b43ad8ae7c6b9117ad7dfceab50
Christopher Peplin <chris.peplin@rhubarbtech.com>
2013-09-24 12:00:16 -0400

    Add CLI flashing docs for Windows and Mac.

--- static/js/app.js
+++ static/js/app.js
@@ -4,4 +4,19 @@ $(document).ready(function() {
             $(value).addClass('active');
         }
     });
+
+    if(window.location.hash) {
+        $(window.location.hash).show();
+    } else {
+        // auto-detect OS
+        var platform = navigator.platform;
+        if(/mac/i.test(platform)) {
+            $("#collapseMac").addClass("in");
+        } else if(/linux/i.test(platform)) {
+            $("#collapseLinux").addClass("in");
+        } else {
+            // default to Windows
+            $("#collapseWindows").addClass("in");
+        }
+    }
 });
